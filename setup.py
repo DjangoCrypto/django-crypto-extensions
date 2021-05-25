ed4bbf637e7c86897bc102b4155282cf9628ace3
Kamil Marczak <kamil1marczak@gmail.com>
2021-05-25 18:51:28 +0200

    innit

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
+    README = readme.read()
+
+with open(
+    os.path.join(os.path.dirname(__file__), "requirements.txt")
+) as requirements_txt:
+    requirements = requirements_txt.read().strip().splitlines()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="django_crypto_extensions",
+    version="0.0.1",
+    packages=["django_crypto_extensions", "django_crypto_extensions.runtests"],
+    include_package_data=True,
+    # extras_require={
+    #     "Base64ImageField": ["Pillow >= 6.2.1"],
+    # },
+    license="Apache-2.0",
+    license_files=["LICENSE"],
+    description="Extension for Django in terms of Cryptography.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="DjangoCrypto",
+    author_email="kamil1marczak@gmail.com",
+    url="https://github.com/DjangoCrypto/django-crypto-extensions",
+    python_requires=">=3.5",
+    install_requires=requirements,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
